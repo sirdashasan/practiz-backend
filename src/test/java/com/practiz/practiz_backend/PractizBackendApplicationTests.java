87a067105fd390e18e5e41c6288527a895cbb3e0
sirdashasan <sirdashasan@hotmail.com>
2025-04-04 15:18:41 +0300

    initial commit

---
+++ src/test/java/com/practiz/practiz_backend/PractizBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practiz.practiz_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PractizBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
